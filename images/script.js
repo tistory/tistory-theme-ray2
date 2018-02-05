20ad7ff79925325038c7e9bf683e27f131916433
Joo <joo.yoon@kakaocorp.com>
2018-02-05 11:40:39 +0900

    1.0

--- images/script.js
+++ images/script.js
@@ -6,8 +6,8 @@
     Area.Menu = (function() {
 
         var $wrap = $(".wrap_skin"),
-            $btnCategory = $(".area_head .btn_cate");
-            $btnCloseCategory =  $(".wrap_sub .btn_close")
+            $btnCategory = $(".area_head .btn_cate"),
+            $btnCloseCategory =  $(".wrap_sub .btn_close");
 
         $btnCategory.on("click", function() {
             $wrap.addClass("navi_on");
@@ -56,52 +56,35 @@
 
     })();
 
-    Area.SocialShare = (function() {
-
-		var SOCIAL_SHARE = {
-			kakaostory: {
-				url: "https://story.kakao.com/share",
-				width: 640,
-				height: 400,
-				makeShareUrl: function(url) {
-					return this.url + "?url=" + encodeURIComponent(url);
-				}
-			},
-			facebook: {
-				url: "https://www.facebook.com/sharer/sharer.php",
-				width: 640,
-				height: 400,
-				makeShareUrl: function(url) {
-					return this.url + "?display=popup&u=" + encodeURIComponent(url);
-				}
-			},
-			twitter: {
-				url: "https://twitter.com/intent/tweet",
-				width: 640,
-				height: 400,
-				makeShareUrl: function(url) {
-					return this.url + "?url=" + encodeURIComponent(url);
-				}
-			}
-		};
-
-
-		var onClick = function(e) {
-			var $this = $(this),
-				service = SOCIAL_SHARE[$this.data("service")],
-				url = location.href;
-
-			if (service) {
-				e.preventDefault();
-				window.open(service.makeShareUrl(url), "", "width=" + service.width + ", height=" + service.height);
-			}
-		};
-
-
-		$(".list_share").on("click", "a", onClick);
-
-	})();
-
     $.Area = Area;
+
+	(function() {
+		var $window = $(window);
+		var $content = $(".area_view");
+
+		var getSize = function(value) {
+			return (value && value.indexOf('%') < 0)? value : null
+		};
+
+		var adjustIframeSize = function() {
+			var contentWidth = $content.width();
+			$content.find("iframe").each(function(i, iframe) {
+				console.log(i, iframe);
+				var $iframe = $(iframe),
+					width = getSize($iframe.attr("width")),
+					height = getSize($iframe.attr("height"));
+
+				if (width && height) {
+					$iframe.css({
+						width: contentWidth + "px",
+						height: contentWidth / width * height + "px"
+					})
+				}
+			})
+		};
+
+		adjustIframeSize();
+		$window.on("orientationchange resize", adjustIframeSize);
+	})()
 
 })(jQuery);
